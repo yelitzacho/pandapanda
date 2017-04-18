1fda05388b50fcfb105b8f063e0948e6e1823faa
Yelitza Cho <yelitza.choque@gmail.com>
2017-04-17 20:12:57 -0500

    Funcion de los botones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,27 @@
+window.addEventListener("load", function(evento){
+	evento.preventDefault();
 
 	var parrafOrigen = document.getElementById("texto1");
 	var parrafoHistoria = document.getElementById("texto2");
 
-	function mostrarOrigen(){
+	var mostrarOrigen = document.getElementById("origen");
+	var mostrarHistoria = document.getElementById("historia");
+
+
+	mostrarOrigen.addEventListener("click", function(){
 		if (parrafOrigen.style.display === "none") {
 			parrafOrigen.style.display = "inline-block";
 		} else {
 			parrafOrigen.style.display = "none";
 		}
-	}
-
-	function mostrarHistoria(){
+	})
+	
+	mostrarHistoria.addEventListener("click", function(){
 		if (parrafoHistoria.style.display === "none") {
 			parrafoHistoria.style.display = "inline-block";
 		} else {
 			parrafoHistoria.style.display = "none";
 		}
-	}
+	})
+
+})
